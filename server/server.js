6d756e6da49cdb19c0135ff874d6efac623ed790
Nikhil Salunkhe <nsalunkhe803@gmail.com>
2026-08-02 12:23:13 +0530

    Update TechMitra chatbot

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ async function getNextSequence(name) {
   const counter = await Counter.findOneAndUpdate(
     { _id: name },
     { $inc: { seq: 1 } },
-    { new: true, upsert: true }
+    { returnDocument: 'after', upsert: true }
   );
   return counter.seq;
 }
